3d582e2cdc29c5f45075cf1159b660ce0a19e955
zouhairkhallaf <zouhairkhallaf@gmail.com>
2017-06-17 02:24:36 -0400

    addded the server variables

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ var api = new ParseServer({
 
 	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
 	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+	serverURL: process.env.SERVER_URL || 'https://my-parse-server-codecraft-ex.herokuapp.com/',  // Don't forget to change to https if needed
 	
 	//**** Security Settings ****//
 	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false, 
 	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!	
+	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
 	
 	//**** Live Query ****//
 	// liveQuery: {
